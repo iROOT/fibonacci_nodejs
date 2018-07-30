2b7b530928a2054bc541fdda965527934fa29a21
Alexandr Mitkalev <iroota@gmail.com>
2018-07-30 22:38:35 +0300

    Add function download file and code refactoring

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const http = require('http');
 const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
+const readFile = promisify(fs.readFile);
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -31,31 +35,88 @@ function factorial(n) {
     return mul;
 }
 
+const downloadFile = async(srvUrl, res) => {
+    let filePath;
+    if (typeof srvUrl.query === 'object' && 'file' in srvUrl.query) {
+        const rootDir = path.resolve(__dirname, 'public');
+        filePath = path.join(rootDir, srvUrl.query['file']);
+        if (filePath.indexOf(rootDir) !== 0) {
+            throw URIError('Directory traversal');
+        }
+    } else {
+        throw URIError("File path is not specified");
+    }
+
+    let extName = path.extname(filePath);
+    let contentType = {
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.txt': 'text/plain',
+        '.html': 'text/html',
+    }[extName] || 'text/plain';
+
+    await readFile(filePath)
+    .then(content => {
+        res.writeHead(200, {'Content-Type': contentType});
+        res.end(content, 'utf8');
+    });
+};
+
 function getResult(funcName, n) {
     if (!Number.isInteger(n)) {
         throw TypeError;
     } else if (n < 0) {
-        throw RangeError;
+        throw RangeError("Input value is negative");
     }
 
-    if (funcName === 'fibonacci') {
-        if (n > 1476) {
-            return Infinity;
-        } else {
-            return fibonacci(n);
-        }
-    } else if (funcName === 'factorial') {
-        if (n > 170) {
-            return Infinity;
-        } else {
-            return factorial(n);
-        }
-    } else {
-        throw URIError;
+    switch (funcName) {
+        case 'fibonacci':
+            if (n > 1476) {
+                return Infinity;
+            } else {
+                return fibonacci(n);
+            }
+        case 'factorial':
+            if (n > 170) {
+                return Infinity;
+            } else {
+                return factorial(n);
+            }
+        default:
+            throw URIError("Function not found");
     }
 }
 
-this.server = http.createServer((req, res) => {
+function getErrorReaction(e) {
+    let res = {
+        "RangeError": {
+            code: 400,
+            message: "Input value is negative"
+        },
+        "URIError": {
+            code: 404,
+            message: "Function not found"
+        },
+        "TypeError": {
+            code: 500,
+            message: "Invalid function argument"
+        },
+        "ENOENT": {
+            code: 404,
+            message: "File not found"
+        }
+    };
+
+    return res[e.name] || {
+        code: 500,
+        message: ''
+    };
+}
+
+this.server = http.createServer(async(req, res) => {
     if (req.url === '/favicon.ico') {
         res.writeHead(200, {'Content-Type': 'image/x-icon'});
         res.end();
@@ -65,27 +126,41 @@ this.server = http.createServer((req, res) => {
     try {
         const srvUrl = url.parse(req.url, true);
 
-        if (typeof srvUrl.query === 'object' && 'i' in srvUrl.query) {
-            let n = parseInt(srvUrl.query['i'], 10);
+        switch (srvUrl.pathname) {
+            case '/fibonacci':
+            case '/factorial':
+                if (typeof srvUrl.query === 'object' && 'i' in srvUrl.query) {
+                    const n = parseInt(srvUrl.query['i'], 10);
 
-            let result = getResult(srvUrl.pathname.slice(1), n).toString();
+                    const result = getResult(srvUrl.pathname.slice(1), n).toString();
 
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(result);
-        } else {
-            throw TypeError;
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    res.end(result);
+                } else {
+                    throw TypeError("Invalid function argument");
+                }
+                break;
+
+            case '/download':
+                await downloadFile(srvUrl, res);
+                break;
+
+            case '/favicon.ico':
+                res.writeHead(200, {'Content-Type': 'image/x-icon'});
+                res.end();
+                break;
+
+            default:
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('404 Page not found\n');
         }
     } catch (e) {
-        if (e.name === "RangeError") {
-            res.statusCode = 400;
-            res.end();
-        } else if (e.name === "URIError") {
-            res.statusCode = 404;
-            res.end();
-        } else if (e.name === "TypeError") {
-            res.statusCode = 500;
-            res.end();
-        }
+        // let {code, message} = getErrorReaction(e.name);
+        let message = `${e.stack}`;
+
+        res.writeHead(code, {'Content-Type': 'text/plain'});
+        res.end(message);
+        console.error(message);
     }
 });
 
@@ -98,13 +173,8 @@ if (process.argv[2] === "server") {
     try {
         console.log(getResult(process.argv[2], parseInt(process.argv[3], 10)));
     } catch (e) {
-        if (e.name === "RangeError") {
-            console.log("Input value is negative");
-        } else if (e.name === "URIError") {
-            console.log("Function not found");
-        } else if (e.name === "TypeError") {
-            console.log("Invalid function argument");
-        }
+        // let [code, message] = getErrorReaction(e);
+        console.error(e.stack);
     }
 }
 
