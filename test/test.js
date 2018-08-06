9de5e8176c9d7b7cbf712bb22714ed54b696c946
Alexandr Mitkalev <iroota@gmail.com>
2018-08-06 11:59:06 +0300

    Refactoring tests

--- test/test.js
+++ test/test.js
@@ -1,180 +1,121 @@
 const server = require('../http-api'),
     {describe, before, after, it} = require('mocha'),
     assert = require('assert'),
-    http = require('http');
+    request = require('request');
 
-describe('/', function () {
-    before(function () {
-        server.listen(8000);
+
+const PORT = 8000;
+const DOMAIN = `http://localhost:${PORT}`;
+
+describe('/', () => {
+    before(() => {
+        server.listen(PORT);
     });
 
-    after(function () {
+    after(() => {
         server.close();
     });
 
-    it('should return code 200', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=5', function (res) {
-            assert.equal(200, res.statusCode);
+    it('should return code 200', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=5`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
             done();
         });
     });
 
-    it('should return code 400', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=-100', function (res) {
-            assert.equal(400, res.statusCode);
+    it('should return code 400', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=-100`, (err, res, body) => {
+            assert.equal(res.statusCode, 400);
         });
 
-        http.get('http://localhost:8000/factorial?i=-100', function (res) {
-            assert.equal(400, res.statusCode);
+        request.get(`${DOMAIN}/factorial?i=-100`, (err, res, body) => {
+            assert.equal(res.statusCode, 400);
             done();
         });
     });
 
-    it('should return code 500', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=abc', function (res) {
-            assert.equal(500, res.statusCode);
+    it('should return code 500', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=abc`, (err, res, body) => {
+            assert.equal(res.statusCode, 500);
         });
 
-        http.get('http://localhost:8000/factorial?i=abc', function (res) {
-            assert.equal(500, res.statusCode);
+        request.get(`${DOMAIN}/factorial?i=abc`, (err, res, body) => {
+            assert.equal(res.statusCode, 500);
             done();
         });
     });
 
 
-    it('Fibonacci 0 should return 0', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=0', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('0', data);
-                done();
-            });
+    it('Fibonacci 0 should return 0', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=0`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '0');
+            done();
         });
     });
 
-    it('Fibonacci 1 should return 1', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=1', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('1', data);
-                done();
-            });
+    it('Fibonacci 1 should return 1', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=1`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '1');
+            done();
         });
     });
 
-    it('Fibonacci 2 should return 1', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=2', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('1', data);
-                done();
-            });
+    it('Fibonacci 2 should return 1', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=2`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '1');
+            done();
         });
     });
 
-    it('Fibonacci 3 should return 2', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=3', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('2', data);
-                done();
-            });
+    it('Fibonacci 3 should return 2', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=3`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '2');
+            done();
         });
     });
 
-    it('Fibonacci 10 should return 55', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=10', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('55', data);
-                done();
-            });
+    it('Fibonacci 10 should return 55', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=10`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '55');
+            done();
         });
     });
 
-    it('Fibonacci 1476 should return 1.3069892237633987e+308', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=1476', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('1.3069892237633987e+308', data);
-                done();
-            });
+    it('Fibonacci 1476 should return 1.3069892237633987e+308', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=1476`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '1.3069892237633987e+308');
+            done();
         });
     });
 
-    it('Fibonacci 1477 should return Infinity', function (done) {
-        http.get('http://localhost:8000/fibonacci?i=1477', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('Infinity', data);
-                done();
-            });
+    it('Fibonacci 1477 should return Infinity', (done) => {
+        request.get(`${DOMAIN}/fibonacci?i=1477`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, 'Infinity');
+            done();
         });
     });
 
 
-    it('Factorial 5 should return 55', function (done) {
-        http.get('http://localhost:8000/factorial?i=5', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('120', data);
-                done();
-            });
+    it('Factorial 5 should return 55', (done) => {
+        request.get(`${DOMAIN}/factorial?i=5`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '120');
+            done();
         });
     });
 
-    it('Factorial 20 should return 2432902008176640000', function (done) {
-        http.get('http://localhost:8000/factorial?i=20', function (res) {
-            let data = '';
-
-            res.on('data', function (chunk) {
-                data += chunk;
-            });
-
-            res.on('end', function () {
-                assert.equal('2432902008176640000', data);
-                done();
-            });
+    it('Factorial 20 should return 2432902008176640000', (done) => {
+        request.get(`${DOMAIN}/factorial?i=20`, (err, res, body) => {
+            assert.equal(res.statusCode, 200);
+            assert.equal(body, '2432902008176640000');
+            done();
         });
     });
 });
